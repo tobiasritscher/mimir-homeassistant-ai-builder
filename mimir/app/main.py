3c3e92374154a07960952b5dce492a398f902146
Tobias Ritscher <tobias@ritscher.ch>
2026-01-11 13:11:19 +0100

    bumbed version

--- mimir/app/main.py
+++ mimir/app/main.py
@@ -49,7 +49,7 @@ logger = get_logger(__name__)
 class MimirAgent:
     """The main Mímir agent application."""
 
-    VERSION = "0.1.27"
+    VERSION = "0.1.28"
 
     def __init__(self) -> None:
         """Initialize the Mímir agent."""
